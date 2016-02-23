e8bdfd00dba8d6c5e7111737b2b7190e4e2825d1
denglert <englert.david@gmail.com>
2016-02-23 17:15:47 +0000

    Compact shell split script

---
+++ shell/examples/split.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+inputfile=$1
+outputbase=$2
+
+nSuffixLength=1
+# Headers
+StartLine=2
+nSplitLines=2
+
+#split -a $nSuffixLength -d -l $nSplitLines $inputfile $outputbase
+#cat $inputfile | split -a $nSuffixLength -d -l $nSplitLines - $outputbase
+tail -n +$StartLine $inputfile | split -a $nSuffixLength -d -l $nSplitLines - $outputbase
+
+# -a suffix length
+# -d numeric suffix
+# -l number of lines
+# See 'split --help'
