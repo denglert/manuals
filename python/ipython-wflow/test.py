5ea1b7a42efb0f205acfcc387fd207c3245b1c97
denglert <englert.david@gmail.com>
2016-11-12 21:33:12 +0000

    vim + iPython workflow

---
+++ python/ipython-wflow/test.py
@@ -0,0 +1,13 @@
+# ipython + vim workflow:
+# https://www.youtube.com/watch?v=k-mAuNY9szI
+# ~/.ipython/profile_default/ipython_config.py
+# Contents:
+# c = get_config()
+# 
+# c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+# c.InteractiveShellApp.extensions = ['autoreload']
+
+
+def testfunction():
+    print("hello")
+    print("cica")
